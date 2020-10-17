4c5d34996ecaf57375b5e8dcbbd2db0175a8d40a
Rahul <dhankharrahul13@gmail.com>
2020-10-17 22:51:14 +0530

    Modified arrays

--- Javascript/arrays.js
+++ Javascript/arrays.js
@@ -6,3 +6,11 @@ console.log(typeof marks);
 marks[2] = 100;
 console.log(marks);
 console.log(marks.length);
+marks.pop()
+console.log(marks)
+marks.shift()
+console.log(marks)
+marks.push(82820);
+console.log(marks);
+marks.unshift(1222222);
+console.log(marks)
